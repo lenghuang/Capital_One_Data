3256631bb61d7624866735647b174ebd910929d1
Len Huang <32604574+lenghuang@users.noreply.github.com>
2019-11-01 22:26:49 -0400

    Python used to gather data

---
+++ titles.py
@@ -0,0 +1,33 @@
+import requests
+import json
+
+url = ("http://jservice.io/api/category?id=")
+id = 1
+final_data = []
+
+'''
+Use a changing url and offset to access all 
+of the api's data. Did this to compensate for
+technical issues with my computer and 
+bundle install.
+'''
+
+# Loop through pages of api and add to list of dicts
+while(id < 18419): #14280 #156709 #18418
+    initial_data = []
+    response = requests.get(url + str(id))
+    initial_data.append(json.loads(response.text))
+    # Skip null ID
+    if (len(initial_data[0].keys()) == 2):
+        id += 1
+    else:
+        del initial_data[0]["clues_count"]   
+        del initial_data[0]["clues"]
+        final_data.append(initial_data[0])
+    if (id % 50 == 0):
+         print(id)
+    id += 1
+
+# Create a file for our new list of dictionaries
+with open('titles.json', 'w') as f:
+        json.dump(final_data, f)
