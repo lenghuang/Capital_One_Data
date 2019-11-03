6c03addae2dddf6159ef4dd62ee1928d5f3b1ab8
Len Huang <32604574+lenghuang@users.noreply.github.com>
2019-11-03 11:30:25 -0500

    Add offset

--- cluecomp.py
+++ cluecomp.py
@@ -27,44 +27,45 @@ while id < 156709: #156709
     answer = json.loads(response.text)
 
     for i in range(0, len(dates)):
-        # Deletes for dates
-        del dates[i]["answer"]
-        del dates[i]["question"]
-        del dates[i]["value"]
-        del dates[i]["category_id"]
-        del dates[i]["created_at"]
-        del dates[i]["updated_at"]
-        del dates[i]["game_id"]
-        del dates[i]["invalid_count"]
-        del dates[i]["category"]
-        comp_dates.append(dates[i])
-        # Deletes for question
-        del question[i]["answer"]
-        del question[i]["airdate"]
-        del question[i]["value"]
-        del question[i]["category_id"]
-        del question[i]["created_at"]
-        del question[i]["updated_at"]
-        del question[i]["game_id"]
-        del question[i]["invalid_count"]
-        del question[i]["category"]
-        comp_question.append(question[i])
-        # Deletes for Answer
-        del answer[i]["question"]
-        del answer[i]["airdate"]
-        del answer[i]["value"]
-        del answer[i]["created_at"]
-        del answer[i]["category_id"]
-        del answer[i]["updated_at"]
-        del answer[i]["game_id"]
-        del answer[i]["invalid_count"]
-        del answer[i]["category"]
-        comp_answer.append(answer[i])
-
+        if(len(dates[i].keys()) == 11):
+                # Deletes for dates
+                del dates[i]["answer"]
+                del dates[i]["question"]
+                del dates[i]["value"]
+                del dates[i]["category_id"]
+                del dates[i]["created_at"]
+                del dates[i]["updated_at"]
+                del dates[i]["game_id"]
+                del dates[i]["invalid_count"]
+                del dates[i]["category"]
+                dates[i].update({"offset" : id})
+                comp_dates.append(dates[i])
+                # Deletes for question
+                del question[i]["answer"]
+                del question[i]["airdate"]
+                del question[i]["value"]
+                del question[i]["category_id"]
+                del question[i]["created_at"]
+                del question[i]["updated_at"]
+                del question[i]["game_id"]
+                del question[i]["invalid_count"]
+                del question[i]["category"]
+                question[i].update({"offset" : id})
+                comp_question.append(question[i])
+                # Deletes for Answer
+                del answer[i]["question"]
+                del answer[i]["airdate"]
+                del answer[i]["value"]
+                del answer[i]["created_at"]
+                del answer[i]["category_id"]
+                del answer[i]["updated_at"]
+                del answer[i]["game_id"]
+                del answer[i]["invalid_count"]
+                del answer[i]["category"]
+                answer[i].update({"offset" : id})
+                comp_answer.append(answer[i])
     id += 100
         
-
-
 # Create a file for our new list of dictionaries
 with open('dates.json', 'w') as f:
         json.dump(comp_dates, f)
